20f6548e8beb3c9c562bcd9572cc947ad69aa17e
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-13 20:18:51 +0200

    Finished block structure of the layout

--- src/components/SiteFooterComponent.vue
+++ src/components/SiteFooterComponent.vue
@@ -2,12 +2,26 @@
     <!-- /#site_footer -->
     <div id="site_footer">
         <!-- Upper footer goes here v -->
-        <div class="container-fluid upper_footer">
-            Upper footer
+        <div class="container-fluid upper_footer cont_cent">
+           <div class="row w-100 h-100 p-0 flex_cent">
+               <!-- Avada Finance -->
+               <div class="col-3 flex_cent h-100 bordo">Avada Finance</div>
+               <!-- Recent Posts -->
+               <div class="col-3 flex_cent h-100 bordo">Recent Posts</div>
+               <!-- Services -->
+               <div class="col-3 flex_cent h-100 bordo">Services</div>
+               <!-- Latest -->
+               <div class="col-3 flex_cent h-100 bordo">Latest</div>
+           </div>
         </div>
         <!-- Under footer goes here v -->
-        <div class="container-fluid under_footer">
-            Under footer
+        <div class="container-fluid under_footer cont_cent">
+            <div class="row w-100 h-100 d-flex justify-content-between align-items-md-center">    
+            <!-- CopyRight Text -->
+            <div class="col-4 h-100 bordo">CopyRight Text</div>
+            <!-- Partner Icons -->
+            <div class="col-2 h-100 bordo">Partner Icons</div>
+            </div>
         </div>
     </div>
 </template>
