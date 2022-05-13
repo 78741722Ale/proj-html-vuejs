d765945674b98c0c1d9af0419a697d35e312afa6
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-13 17:36:03 +0200

    Completed indicative site header blocks

--- src/components/SiteHeaderComponent.vue
+++ src/components/SiteHeaderComponent.vue
@@ -2,19 +2,33 @@
     <!-- /#site_header -->  
     <div id="site_header">
         <!-- navbar goes here v -->
-        <div class="container navbar_site">
-            Navbar
+        <div :class="nav_cont">
+            <div :class="nav_row">
+                <!-- Logo Avada -->
+                <div class="col-3 h-100 bg-info bordo">Logo</div>
+                <!-- Navbar -->
+                <div class="col-6 h-100 bg-warning bordo">Navbar</div>
+            </div>
         </div>
         <!--Jumbotron goes here v -->
-        <div class="container-fluid jumbotron_site">
-            Jumbotron
-        </div>
+        <div class="container-fluid jumbotron_site">Jumbotron</div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'SiteHeaderComponent'
+    name: 'SiteHeaderComponent',
+    
+    data() {
+        return {
+            /* Classi Dinamiche - Navbar */
+            nav_cont: "container d-flex justify-content-center navbar_site p-0",
+            nav_row: "row d-flex justify-content-between align-content-center w-100 h-100 bg-danger p-0"
+        }
+    },
+
+
+
 }
 </script>
 
