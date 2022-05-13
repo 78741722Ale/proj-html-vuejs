fd5cd9e25b855064e16caf293728d20512c25c70
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-13 11:20:45 +0200

    Added Bootstrap via npm and linked on file main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+/* Import di Bootstrap */
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const bootstrap = require('bootstrap')
+Vue.use(bootstrap)
+
 
 Vue.config.productionTip = false
 
