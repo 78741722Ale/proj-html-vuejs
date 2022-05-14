abcc228ef909457afa677c1c6aa1f1c8ef80650d
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-14 15:10:08 +0200

    concluded footer component (non-dynamic).

--- src/components/SiteFooterComponent.vue
+++ src/components/SiteFooterComponent.vue
@@ -4,13 +4,32 @@
         <!-- Upper footer goes here v -->
         <div class="container-fluid upper_footer cont_cent">
            <div class="row w-100 h-100 p-0 flex_cent">
-               <!-- Avada Finance -->
-               <div class="col-3 flex_cent h-100 bordo">Avada Finance</div>
+               <!-- Avada Finance - Larghezza da sistemare -->
+               <div class="col-3 flex_cent h-100">
+                   <img src="@/assets/img/finance_logo_footer.png" alt="Avada Finance Logo" srcset="">
+               </div>
                <!-- Recent Posts -->
-               <div class="col-3 flex_cent h-100 bordo">Recent Posts</div>
+               <div class="col-3 ps-3 d-flex justify-content-center align-items-start flex-column p-0 h-100 bordo">
+                   <span class="pb-2"><strong class="text-uppercase">Recent post</strong></span>
+                   <!-- Lista di Recent Post -->
+                   <ul class="foot_lists ps-0">
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Lorem ipsum dolor sit.</li>
+                   </ul>
+               </div>
                <!-- Services -->
-               <div class="col-3 flex_cent h-100 bordo">Services</div>
+               <div class="col-3 ps-3 d-flex justify-content-center align-items-start flex-column p-0 h-100 bordo">
+                   <span class="pb-2"><strong class="text-uppercase">Services</strong></span>
+                   <!-- Lista di Recent Post -->
+                   <ul class="foot_lists ps-0">
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Lorem ipsum dolor sit.</li>
+                   </ul>                   
+               </div>
                <!-- Latest -->
+               <!-- Da concludere graficamente con icone -->               
                <div class="col-3 flex_cent h-100 bordo">Latest</div>
            </div>
         </div>
