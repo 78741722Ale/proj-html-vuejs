0ffa51da6cdb4107cd28e6c182739b233b0dee5d
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-14 17:12:40 +0200

    Added correct font (most similar found on fontsource).

--- src/main.js
+++ src/main.js
@@ -6,10 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 /* Import del font da fontsource */
-import "@fontsource/josefin-sans"
-import "@fontsource/josefin-sans/100.css"
-import "@fontsource/josefin-sans/300.css"
-import "@fontsource/josefin-sans/500.css"
+import "@fontsource/pt-sans"
+import "@fontsource/josefin-sans/400.css"
 import "@fontsource/josefin-sans/700.css"
 
 Vue.config.productionTip = false
