e77ace6df6e7ea3c50105855dc689c5f4c3e2a26
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-15 19:29:29 +0200

    Completed the Feedback component with dynamic classes.
    Added new classes and regions to main.scss

--- src/components/main/FeedbackComponent.vue
+++ src/components/main/FeedbackComponent.vue
@@ -1,16 +1,23 @@
 <template>
-        <!-- Client Feedback goes here v - CO -->
-        <!-- Non ancora concluso -->          
-        <div class="container-fluid feedback cont_cent">
-            <div class="row w-100 h-100 p-0 flex_cent">
-                <h2>Client Feedback</h2>
-            </div>        
-        </div> 
+    <!-- Client Feedback -->        
+    <div :class="feedback_cont">
+        <div :class="feedback_row">
+            <h2 :class="feedback_style">Client Feedback</h2>
+        </div>        
+    </div> 
 </template>
 
 <script>
 export default {
-    name:'FeedbackComponent'
+    name:'FeedbackComponent',
+    data() {
+        return {
+        /* Classi dinamiche */
+        feedback_cont: "container-fluid feedback cont_cent",
+        feedback_row: "row w-100 h-100 p-0 flex_cent",
+        feedback_style: "title_feedback pb-3 w-25",
+        }
+    },
 }
 </script>
 
