df7c924458d0152a389625b72d816cbe150b0495
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-15 19:51:16 +0200

    Added v-for loop to ListOfFeedback component using v-for directive.
    Added object array for directive.

--- src/components/main/ListOfFeedbackComponent.vue
+++ src/components/main/ListOfFeedbackComponent.vue
@@ -1,106 +1,20 @@
 <template>
         <!-- List cards of Feedback goes here v - CO -->
         <!-- Tutto contenuto dinamico le col sono da estrarre in v-for -->
-        <div class="container-fluid cards_feed cont_cent">
-            <div class="row flex_cent p-0 h-100 w-100">
-                <!-- Event Elevate -->
-                <div class="col-3 h-100 p-0 flex_cent flex-column">
+        <div :class="listOfFeedback_cont">
+            <div :class="listOfFeedback_row">
+                <!-- Cards - Qua il v-for -->
+                <div v-for="(element, index) in raccomandations" :key="index" :class="listOfFeedback_single_dim">
                     <!-- Logo Aziendale e testo here -->
-                    <!-- Utilizza v-for dinamico -->
-                    <div class="col-12 h-75 bordo_feedback p-0 flex_cent flex-column">
-                        <!-- Logo -->
-                        <div class="col-6 h-25 flex_cent">
-                            <img src="@/assets/img/client-logo-4-200x87.png" alt="Event Elevate" srcset="">
-                        </div>
-                        <!-- Raccomandation -->                         
-                        <div class="col-9 h-50 flex_cent">
-                            <p>
-                                "Avada Finance offers a fantastic service! It is so easy to use and the
-                                customer support is the best around. This payment option is my number one
-                                choice. I always recommend it."
-                            </p>
-                        </div>
+                    <div :class="listOfFeedback_upper_zone">
+                        <!-- Logo & Raccomandation -->
+                        <div class="col-6 h-25 flex_cent"><img :src="element.logo_part" :alt="element.partner"></div>                        
+                        <div class="col-9 h-50 flex_cent"><p>{{element.text}}</p></div>
                     </div>
-                    <!-- Citazione goes here -->
-                    <!-- Utilizza v-for dinamico -->                    
-                    <div class="col-12 h-25 bordo_feedback p-0 flex_cent flex-column">
-                        <span class="text-uppercase"><strong>Andre Mendoza</strong></span>
-                        <span class="text-uppercase"><strong>Creative Market</strong></span>
-                    </div>                    
-                </div>
-                <!-- Bonjour Cafe -->
-                <div class="col-3 h-100 p-0 flex_cent flex-column">
-                    <!-- Logo Aziendale e testo here -->
-                    <!-- Utilizza v-for dinamico -->
-                    <div class="col-12 h-75 bordo_feedback p-0 flex_cent flex-column">
-                        <!-- Logo -->
-                        <div class="col-6 h-25 flex_cent">
-                            <img src="@/assets/img/client-logo-1-1-200x87.png" alt="Event Elevate" srcset="">
-                        </div>
-                        <!-- Raccomandation -->                         
-                        <div class="col-9 h-50 flex_cent">
-                            <p>
-                                "Avada Finance offers a fantastic service! It is so easy to use and the
-                                customer support is the best around. This payment option is my number one
-                                choice. I always recommend it."
-                            </p>
-                        </div>
-                    </div>
-                    <!-- Citazione goes here -->
-                    <!-- Utilizza v-for dinamico -->                    
-                    <div class="col-12 h-25 bordo_feedback p-0 flex_cent flex-column">
-                        <span class="text-uppercase"><strong>Andre Mendoza</strong></span>
-                        <span class="text-uppercase"><strong>Creative Market</strong></span>
-                    </div>                    
-                </div>
-                <!-- Futuristic  -->
-                <div class="col-3 h-100 p-0 flex_cent flex-column">
-                    <!-- Logo Aziendale e testo here -->
-                    <!-- Utilizza v-for dinamico -->
-                    <div class="col-12 h-75 bordo_feedback p-0 flex_cent flex-column">
-                        <!-- Logo -->
-                        <div class="col-6 h-25 flex_cent">
-                            <img src="@/assets/img/client-logo-2-200x87.png" alt="Futuristic" srcset="">
-                        </div>
-                        <!-- Raccomandation -->                         
-                        <div class="col-9 h-50 flex_cent">
-                            <p>
-                                "Avada Finance offers a fantastic service! It is so easy to use and the
-                                customer support is the best around. This payment option is my number one
-                                choice. I always recommend it."
-                            </p>
-                        </div>
-                    </div>
-                    <!-- Citazione goes here -->
-                    <!-- Utilizza v-for dinamico -->                    
-                    <div class="col-12 h-25 bordo_feedback p-0 flex_cent flex-column">
-                        <span class="text-uppercase"><strong>Andre Mendoza</strong></span>
-                        <span class="text-uppercase"><strong>Creative Market</strong></span>
-                    </div>                    
-                </div>
-                <!-- Electricity -->
-                <div class="col-3 h-100 p-0 flex_cent flex-column">
-                    <!-- Logo Aziendale e testo here -->
-                    <!-- Utilizza v-for dinamico -->
-                    <div class="col-12 h-75 bordo_feedback p-0 flex_cent flex-column">
-                        <!-- Logo -->
-                        <div class="col-6 h-25 flex_cent">
-                            <img src="@/assets/img/client-logo-3-200x87.png" alt="Electricity" srcset="">
-                        </div>
-                        <!-- Raccomandation -->                         
-                        <div class="col-9 h-50 flex_cent">
-                            <p>
-                                "Avada Finance offers a fantastic service! It is so easy to use and the
-                                customer support is the best around. This payment option is my number one
-                                choice. I always recommend it."
-                            </p>
-                        </div>
-                    </div>
-                    <!-- Citazione goes here -->
-                    <!-- Utilizza v-for dinamico -->                    
-                    <div class="col-12 h-25 bordo_feedback p-0 flex_cent flex-column">
-                        <span class="text-uppercase"><strong>Andre Mendoza</strong></span>
-                        <span class="text-uppercase"><strong>Creative Market</strong></span>
+                    <!-- Citazione Aziendale e autore -->                   
+                    <div :class="listOfFeedback_under_zone">
+                        <span class="text-uppercase"><strong>{{element.author}}</strong></span>
+                        <span class="text-uppercase"><strong>{{element.role}}</strong></span>
                     </div>                    
                 </div>
             </div>
@@ -109,7 +23,53 @@
 
 <script>
 export default {
-    name:'ListOfFeedbackComponent'
+    name:'ListOfFeedbackComponent',
+    data() {
+        return {
+                /* Classi Dinamiche Vanno Qua v */
+                listOfFeedback_cont : "container-fluid cards_feed cont_cent",
+                listOfFeedback_row : "row flex_cent p-0 h-100 w-100", 
+                /* Classi Dinamiche Singola Card */                
+                listOfFeedback_single_dim : "col-3 h-100 p-0 flex_cent flex-column", 
+                listOfFeedback_upper_zone :"col-12 h-75 p-0 flex_cent flex-column bordo_feed_up",
+                listOfFeedback_under_zone :"col-12 h-25 bordo_feed_down p-0 flex_cent flex-column",                
+                /* Array di oggetto per le raccomandazioni */ 
+                raccomandations : [
+                /* 1° oggetto -  Event Elevate */ 
+                {   
+                    partner:"Event Elevate",
+                    logo_part : require("@/assets/img/client-logo-4-200x87.png"),
+                    text: "'Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it.'",
+                    author:"Andre Mendoza",
+                    role:"Creative Market"
+                },
+                /* 2° oggetto - Bonjour Cafe  */ 
+                {   
+                    partner:"Bonjour Cafe",
+                    logo_part : require("@/assets/img/client-logo-1-1-200x87.png"),
+                    text: "Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it.",
+                    author:"Andre Mendoza",
+                    role:"Creative Market"
+                },
+                /* 3° oggetto - Futuristic */ 
+                {   
+                    partner:"Futuristic",
+                    logo_part : require("@/assets/img/client-logo-2-200x87.png"),
+                    text: "Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it.",
+                    author:"Andre Mendoza",
+                    role:"Creative Market"
+                },
+                /* 4° oggetto - Electricity  */ 
+                {   
+                    partner:"Electricity",
+                    logo_part : require("@/assets/img/client-logo-3-200x87.png"),
+                    text: "Avada Finance offers a fantastic service! It is so easy to use and the customer support is the best around. This payment option is my number one choice. I always recommend it.",
+                    author:"Andre Mendoza",
+                    role:"Creative Market"
+                },
+            ]           
+        }
+    },
 }
 </script>
 
