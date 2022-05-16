93bdd1c75603990d44237e2d19cd4cd75da1b875
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-16 11:09:26 +0200

    Concluded dynamic component CTA
    Data injected via v-for directive and arrays / objects.

--- src/components/main/CTAComponent.vue
+++ src/components/main/CTAComponent.vue
@@ -1,16 +1,50 @@
 <template>
-        <!-- Call to action goes here v -->
-        <!-- Non ancora concluso -->     
-        <div class="container-fluid flex_cent cta_site p-0">
-            <div class="row w-100 h-100">
-                <img class="rtt_img p-0" src="@/assets/img/ready_to_talk_cta_bg.jpg" alt="Ready to talk" srcset="">
+        <!-- Call to action -->    
+        <div :class="CTA_cont">
+            <div v-for="(element, index) in CTA_image" :key="index" :class="CTA_row">
+                <!-- Immagine di sfondo call to action -->
+                <img  class="rtt_img p-0" :src="element.img" :alt="element.descr">
+                <!-- Dati Descrittivi della call to action -->
+                <div v-for="(element, index) in CTA_data" :key="index" :class="CTA_class">
+                    <!-- Titolo -->
+                    <h2 class="pb-2">{{element.title}}</h2>
+                    <!-- Paragrafo Lorem -->  
+                    <span class="w-75 pb-4">{{element.descr}}</span> 
+                    <!-- Button Let's Talk --> 
+                    <button class="btn btn-primary btn_contact">{{element.button}}</button>                
+                </div>
             </div>
         </div>  
 </template>
 
 <script>
 export default {
-    name:'CTAComponent'
+    name:'CTAComponent',
+    /* Dati Dinamici */
+    data() {
+        return {
+            /* Classi dinamiche per il container e per la row */
+            CTA_cont : "container-fluid flex_cent cta_site p-0",
+            CTA_row : "row w-100 h-100 position-relative", 
+            CTA_class:"abs col-5 p-0 h-50 text-center CTA_data",  
+            /* Dati Dinamici - Immagine */  
+            CTA_image : [
+                /* Immagine call to action */
+                {
+                img : require("@/assets/img/ready_to_talk_cta_bg.jpg"),
+                descr : "Ready to talk"
+                }
+            ],
+            CTA_data : [
+                /* Data da iniettare nella descrizione contenuto */
+                {
+                    title:"Ready To Talk?",
+                    descr:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus tempora et deserunt quidem sed. Animi?",
+                    button:"Let's Talk"
+                }
+            ]       
+        }
+    },
 }
 </script>
 
--- src/components/main/ListOfFeedbackComponent.vue
+++ src/components/main/ListOfFeedbackComponent.vue
@@ -9,7 +9,7 @@
                     <div :class="listOfFeedback_upper_zone">
                         <!-- Logo & Raccomandation -->
                         <div class="col-6 h-25 flex_cent"><img :src="element.logo_part" :alt="element.partner"></div>                        
-                        <div class="col-9 h-50 flex_cent"><p class="lofeed_text">{{element.text}}</p></div>
+                        <div class="col-9 h-50 flex_cent"><p class="lofeed_text pt-5">{{element.text}}</p></div>
                     </div>
                     <!-- Citazione Aziendale e autore -->                   
                     <div :class="listOfFeedback_under_zone">
