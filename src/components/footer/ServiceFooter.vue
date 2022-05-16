06637d850cf7096aae1682b8579d5effccbea694
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-16 14:30:06 +0200

    Completed dynamic content of the Service Footer component
    Added data and dynamic classes to it

--- src/components/footer/ServiceFooter.vue
+++ src/components/footer/ServiceFooter.vue
@@ -1,19 +1,43 @@
 <template>
-            <!-- Services -->
-            <div class="col-3 ps-3 d-flex justify-content-center align-items-start flex-column p-0 h-100 bordo">
-                   <span class="pb-2"><strong class="text-uppercase">Services</strong></span>
-                   <!-- Lista di Recent Post -->
-                   <ul class="foot_lists ps-0">
-                        <li>Lorem ipsum dolor sit.</li>
-                        <li>Lorem ipsum dolor sit.</li>
-                        <li>Lorem ipsum dolor sit.</li>
-                   </ul>                   
-            </div>
+    <!-- Services -->
+    <div :class="serv_pos_cont">
+        <span class="pb-2"><strong class="text-uppercase rec_title">{{serv_post_title}}</strong></span>
+        <!-- Lista di Recent Post -->
+        <ul class="serv_lists ps-0">
+            <li v-for="(element, index) in serv_post_elements" :key="index"><font-awesome-icon class="pe-1" :icon="element.icon" />{{element.text}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-    name:"ServiceFooter"
+    name:"ServiceFooter",
+    data() {
+        return {
+            /* Classi dinamiche */
+            serv_pos_cont : "col-3 ps-3 pt-3 d-flex justify-content-center align-items-start flex-column p-0 h-100",
+            /* Contenuto Dinamico */
+            serv_post_title : "Services",
+            serv_post_elements : [
+                /* Small.. */
+                {
+                    icon : "fa-solid fa-chevron-right",
+                    text : "Small Business Loan Services"
+                },
+                /* Credit.. */
+                {
+                    icon : "fa-solid fa-chevron-right",
+                    text : "Credit Rating Advice Services"
+                }, 
+                /* Crowd.. */
+                {
+                    icon : "fa-solid fa-chevron-right",
+                    text : "Crowd Funding Services"
+                },                               
+
+            ]
+        }
+    },
 }
 </script>
 
