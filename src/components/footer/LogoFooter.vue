e0db4d17dfe3a04267acba1f5cf1f3186cbb669c
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-16 13:58:22 +0200

    Dynamic footer logo completed

--- src/components/footer/LogoFooter.vue
+++ src/components/footer/LogoFooter.vue
@@ -1,13 +1,25 @@
 <template>
-    <!-- Avada Finance - Larghezza da sistemare -->
-    <div class="col-3 flex_cent h-100">
-        <img src="@/assets/img/finance_logo_footer.png" alt="Avada Finance Logo" srcset="">
-    </div>
+    <!-- Avada Finance logo -->
+    <div :class="flogo_col">
+        <img v-for="(element, index) in footer_elements" class="logo_footer pt-5" :key="index" :src="element.img" :alt="element.desc">
+    </div> 
 </template>
 
 <script>
 export default {
-    name:"LogoFooter"
+    name:"LogoFooter",
+    data() {
+        return {
+        /* Classe dinamica */
+        flogo_col:"col-3 d-flex justify-content-center align-items-start h-100",
+        footer_elements: [
+                {
+                img: require("@/assets/img/finance_logo_footer.png"),
+                desc: "Avada Finance Logo"
+                }
+            ]
+        }
+    },
 }
 </script>
 
